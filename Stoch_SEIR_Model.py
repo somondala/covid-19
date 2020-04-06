069a7dcb44f78d5acf92c7017866bfc1b9191c7c
somondala <63218870+somondala@users.noreply.github.com>
2020-04-06 15:36:42 -0500

    Update Stoch_SEIR_Model.py
    Optimize code to remove np.append and np.vstack

--- Stoch_SEIR_Model.py
+++ Stoch_SEIR_Model.py
@@ -14,7 +14,7 @@ import datetime as dt
 #matplotlib inline
 #from IPython.display import display, HTML
 # from Stoch_Iteration import Stoch_Iteration
-def update(old_pop, Parameters, change):
+def update(old_pop, Parameters, change, rate):
     
     #dt = 0.01
     #new_pop = np.array([49995, 1, 1, 0, 0, 0])
@@ -37,7 +37,7 @@ def update(old_pop, Parameters, change):
     R = old_pop[5]
     
     
-    rate = np.zeros([9])    
+       
     
     rate[0] = beta*S*(I+delta1*A)/N 
     rate[1] = p_s*kappa*E
@@ -87,87 +87,80 @@ def Stoch_Iteration(Time, Initial, Parameters):
     change[7,:]=[0, 0, 0, 0, -1, +1]
     change[8,:]=[0, 0, 0, 0, -1, +1]
     
-    T = np.array([0.])
-    #T = np.zeros([5*N,1])
-    #pop = np.zeros([5*N,6])
-    pop = [S, E, I, A, Q, R]
-    old_pop = np.array([S, E, I, A, Q, R])  
+    T = np.array(([0.],[0.]), dtype=float)
+    pop = np.zeros([2,6])
+    pop[0,:] = [S, E, I, A, Q, R]
+    old_pop = pop[0,:]
     j = 0
+    rate = np.zeros([9]) 
     
     while (T[j]<Time[1]):
-        [dt, new_pop] = update(old_pop, Parameters, change)
+        [dt, new_pop] = update(old_pop, Parameters, change, rate)
         j = j + 1
-        T = np.append(T, [T[j-1]+dt])
-        #T[j] = T[j-1]+dt
-        #pop[j,:] = new_pop
-        pop = np.vstack((pop, new_pop))
-        old_pop = new_pop
+        #T = np.append(T, [T[j-1]+dt])
+        T[j] = T[j-1]+dt
+        pop[j,:] = new_pop
+        #pop = np.append((pop, new_pop))
+        old_pop = new_pop       
+        if j==(np.size(T)-1):
+            T = np.append(T,np.zeros_like(T))
+            pop = np.append(pop, np.zeros_like(pop),axis=0)
                  
-    return T, pop
+    return T[1:j], pop[1:j,:]
 
-if __name__ == "__main__":
+#############################################################################
         
-    print( 'Updated ', dt.datetime.now())
-    
-    beta = 2.34      # contact rate per person per day
-    kappa = 1/5.     # Incubation period = 5 days
-    p_s = 0.80       # fraction of symptomatic I
-    delta1 = 0.5     # fraction of reduced infectivity of asymptomatic I
-    gamma1 = 1/14.   # Recovery time of Is = 14 days
-    gamma2 = gamma1   # Recovery time of Ia = 14 days
-    mu1 = 0.002      # 0.2% fatality rate per infected person per day
-    mu2 = 0.01       # 1% fatality rate of quarantined person  
-    
-    theta = 0 #1/4.     # if time testing is 4 days, rate of testing and quarantine
-    gamma3 = 1/14.   # recovery time in quarantine = 14 days change
+print( 'Updated ', dt.datetime.now())
 
-    
-    
-    # initial total population size
-    N = 20000
-    
-    # Intial number of infected individuals
-    I0 = 10
-    
-    E0 = 0
-    A0 = 0
-    Q0 = 0
-    S0 = N - I0
-    R0 = N - S0 - I0 - A0- Q0
-    
-    # Total simulation time, in days
-    maxTime = 50
-    
-    #k = 0
-    
-    Parameters = [beta, delta1, gamma1, theta, kappa, mu1, p_s, gamma3, mu2, N]
-    
-    
-    [T, pop] = Stoch_Iteration([0, maxTime], [S0, E0, I0, A0, Q0, R0], Parameters)
-    
-    S = pop[:,0]
-    E = pop[:,1]
-    I = pop[:,2]
-    A = pop[:,3]
-    Q = pop[:,4]
-    R = pop[:,5]
-    
-    plt.figure (1)
-    plt.plot(T,I, label="Infected Symptomatic")
-    plt.plot(T,A, label="Infected Asymptomatic")
-    plt.plot(T,Q, label="Quarantined")
-    plt.xlabel('Time [days]')
-    plt.ylabel('Number of Individuals')
-    plt.title('Simulation of SEIAQR Model')
-    plt.legend(loc='upper right')
-    plt.show()
-    
-    x=5
-    
-    
+beta = 2.34      # contact rate per person per day
+kappa = 1/5.     # Incubation period = 5 days
+p_s = 0.80       # fraction of symptomatic I
+delta1 = 0.5     # fraction of reduced infectivity of asymptomatic I
+gamma1 = 1/14.   # Recovery time of Is = 14 days
+gamma2 = gamma1   # Recovery time of Ia = 14 days
+mu1 = 0.01      # 0.2% fatality rate per infected person per day
+mu2 = 0.01       # 1% fatality rate of quarantined person  
 
-    
+theta = 1/4.     # if time testing is 4 days, rate of testing and quarantine
+gamma3 = 1/14.   # recovery time in quarantine = 14 days change
 
-    
-    
-    
+
+
+# initial total population size
+N = 20000
+
+# Intial number of infected individuals
+I0 = 10
+
+E0 = 0
+A0 = 0
+Q0 = 0
+S0 = N - I0
+R0 = N - S0 - I0 - A0- Q0
+
+# Total simulation time, in days
+maxTime = 50
+
+#k = 0
+
+Parameters = [beta, delta1, gamma1, theta, kappa, mu1, p_s, gamma3, mu2, N]
+
+
+[T, pop] = Stoch_Iteration([0, maxTime], [S0, E0, I0, A0, Q0, R0], Parameters)
+
+S = pop[:,0]
+E = pop[:,1]
+I = pop[:,2]
+A = pop[:,3]
+Q = pop[:,4]
+R = pop[:,5]
+
+plt.figure (1)
+plt.plot(T,I, label="Infected Symptomatic")
+plt.plot(T,A, label="Infected Asymptomatic")
+plt.plot(T,Q, label="Quarantined")
+plt.xlabel('Time [days]')
+plt.ylabel('Number of Individuals')
+plt.title('Simulation of SEIAQR Model')
+plt.legend(loc='upper right')
+plt.show()
